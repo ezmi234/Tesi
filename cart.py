de7b133be4c777d0cc929b6b60cc12386f74dfc3
e.deniku <e.deniku@trust-itservices.com>
2023-10-04 10:55:36 +0200

    [cart.py] Improve decision tree classifier visualization and options.

--- cart.py
+++ cart.py
@@ -4,6 +4,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
+from scipy import stats
 
 df = pd.read_csv("data_banknote_authentication.txt", header=None,
                     names=['variance', 'skewness', 'curtosis', 'entropy', 'class'])
@@ -13,7 +14,17 @@ y = df.iloc[:, 4]
 
 X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=1)
 
-clf = DecisionTreeClassifier(max_depth=5)
+# gini index min
+# clf = DecisionTreeClassifier(max_depth=5)
+
+# gini index random
+clf = DecisionTreeClassifier(splitter='random', max_depth=5)
+
+# entropy min
+# clf = DecisionTreeClassifier(criterion='entropy', max_depth=5)
+
+# entropy random
+# clf = DecisionTreeClassifier(criterion='entropy', splitter='random', max_depth=5)
 
 clf.fit(X_train, y_train)
 
@@ -23,7 +34,7 @@ print("Accuracy:", accuracy_score(y_test, y_pred))
 
 # figure fontsize 8
 plt.figure(figsize=(20, 12))
-tree.plot_tree(clf, fontsize=8)
+tree.plot_tree(clf, feature_names=['variance', 'skewness', 'curtosis', 'entropy'], class_names=['0', '1'], fontsize=8)
 plt.show()
 
 
